46c0e79e4bd625476baf914d0b616e93e2c21620
unknown <amitbiswas9282@gmail.com>
2024-07-16 01:13:06 +0600

    This is Login Page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,74 @@
 import Image from "next/image";
 
 export default function Home() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const number = form.number.value;
+    const password = form.password.value;
+    console.log(name, email, number, password);
+  };
   return (
     <main>
-      <div>Hellow World</div>
+      <h2 className="text-4xl text-center">Please Login!...</h2>
+      <div className="hero bg-base-200 min-h-screen">
+        <form onSubmit={handleSubmit} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="name"
+              placeholder="Please type your Name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Mobile</span>
+            </label>
+            <input
+              type="number"
+              placeholder="Please type your Number"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Please type your Email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="star"
+              placeholder="password"
+              className="input input-bordered"
+              required
+            />
+            {/* <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label> */}
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+      </div>
     </main>
   );
 }
